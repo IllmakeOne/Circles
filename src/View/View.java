cdfe9bc35c352adf9c362d4469947002cf2c35f6
My Lenovo <My Lenovo@Lenovo>
2018-01-16 12:39:30 +0100

    addd view interface

---
+++ src/View/View.java
@@ -0,0 +1,11 @@
+package View;
+
+import Ringz.*;
+
+public interface View {
+	public void display();
+	public void outOfPieces();
+	public Move askMove();
+	public void showPieces();
+	public void notAbletoPlay();
+}
