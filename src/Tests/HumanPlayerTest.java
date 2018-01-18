091045a40be1e44d601108a8d9b5988cd8210999
My Lenovo <My Lenovo@Lenovo>
2018-01-18 14:02:38 +0100

    checkstyle

--- src/Tests/HumanPlayerTest.java
+++ src/Tests/HumanPlayerTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import Players.HumanPalyer;
-import Players.Player;
 import Ringz.Board;
 import Ringz.Color;
 
@@ -26,7 +25,7 @@ class HumanPlayerTest {
 		int[][] pieces = new int[2][5];
 		for (int i = 0; i < 5; i++) {
 			pieces[0][i] = 3;
-			assertEquals(pieces[0][i],playervs3.getPieces()[0][i]);
+			assertEquals(pieces[0][i], playervs3.getPieces()[0][i]);
 		}
 		assertEquals("YELLOW", playervs3.getColor()[0].toString());
 		assertEquals("the best playa ewaa", playervs3.getName());	
@@ -37,8 +36,8 @@ class HumanPlayerTest {
 		for (int i = 0; i < 5; i++) {
 			pieces[0][i] = 3;
 			pieces[1][i] = 1;
-			assertEquals(pieces[0][i],playervs2.getPieces()[0][i]);
-			assertEquals(pieces[1][i],playervs2.getPieces()[1][i]);
+			assertEquals(pieces[0][i], playervs2.getPieces()[0][i]);
+			assertEquals(pieces[1][i], playervs2.getPieces()[1][i]);
 		}
 		assertEquals("GREEN, YELLOW", playervs2.getColor()[0].toString() +
 				", " + playervs2.getColor()[1].toString());
@@ -50,8 +49,8 @@ class HumanPlayerTest {
 		for (int i = 0; i < 5; i++) {
 			pieces[0][i] = 3;
 			pieces[1][i] = 3;
-			assertEquals(pieces[0][i],playervs1.getPieces()[0][i]);
-			assertEquals(pieces[1][i],playervs1.getPieces()[1][i]);
+			assertEquals(pieces[0][i], playervs1.getPieces()[0][i]);
+			assertEquals(pieces[1][i], playervs1.getPieces()[1][i]);
 		}
 		assertEquals("GREEN, YELLOW", playervs1.getColor()[0].toString() +
 				", " + playervs1.getColor()[1].toString());
@@ -89,20 +88,20 @@ class HumanPlayerTest {
 	@Test
 	void testDecreasePiece() {
 		playervs3.decresePiece(0, 0);
-		assertEquals(playervs3.getPieces()[0][0],2);
+		assertEquals(playervs3.getPieces()[0][0], 2);
 		playervs3.decresePiece(0, 0);
-		assertEquals(playervs3.getPieces()[0][0],1);
+		assertEquals(playervs3.getPieces()[0][0], 1);
 		playervs3.decresePiece(0, 0);
-		assertEquals(playervs3.getPieces()[0][0],0);
+		assertEquals(playervs3.getPieces()[0][0], 0);
 		playervs3.decresePiece(0, 3);
-		assertEquals(playervs3.getPieces()[0][3],2);
+		assertEquals(playervs3.getPieces()[0][3], 2);
 		
 		playervs2.decresePiece(1, 3);
-		assertEquals(playervs2.getPieces()[1][3],0);
+		assertEquals(playervs2.getPieces()[1][3], 0);
 	}
 	@Test
-	void TestDetermineMove() {
-		assertEquals(1,1);
+	void testDetermineMove() {
+		assertEquals(1, 1);
 		//not needed
-		}
+	}
 }
