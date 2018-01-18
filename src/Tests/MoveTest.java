33570ac7d9aafeb0d6b60433a4e49e55f9f9aff6
My Lenovo <My Lenovo@Lenovo>
2018-01-18 12:44:57 +0100

    MoveTest

---
+++ src/Tests/MoveTest.java
@@ -0,0 +1,29 @@
+package Tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import Ringz.Color;
+import Ringz.Move;
+
+class MoveTest {
+	private Move mastermove;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		mastermove = new Move(new int[] {2, 3, 1}, Color.BLUE);
+	}
+
+	@Test
+	void test() {
+		assertEquals(3, mastermove.getLine());
+		assertEquals(1, mastermove.getColumn());
+		assertEquals(2, mastermove.getCircle());
+		assertEquals(Color.BLUE, mastermove.getColor());
+		
+		assertEquals(mastermove.toString(),"Line 3 Column  1 Circle sieze 2 Color: BLUE");
+	}
+
+}
